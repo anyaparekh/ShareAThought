01e2a4a6ec3faaa6e9dde38e3888c9fae30b3fda
Anya Parekh <anzparekh@gmail.com>
2024-09-26 17:10:02 -0500

    prep for frontend work

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-[#F8F0E2] w-full h-full justify-center items-center">
+      <Text className="font-montserrat">Hello!</Text>
+    </View>
+  );
+}
