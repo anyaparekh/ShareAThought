3183dac0f09fae900ba3d9b8c185ff49f79c67f9
1arijz <arizukerman1@gmail.com>
2024-09-28 00:52:27 -0500

    Deconstructed props and made noteBody just one string

--- components/NoteCard.tsx
+++ components/NoteCard.tsx
@@ -2,18 +2,17 @@ import { View, Text } from "react-native";
 
 export interface NoteCardProps {
     noteTitle: string;
-    noteBody: string[];
+    noteBody: string;
     bgColor: string;
     textColor: string;
 }
 
-function NoteCard(props: NoteCardProps) {
+function NoteCard({noteTitle, noteBody, bgColor, textColor}: NoteCardProps) {
     return (
-        <View className={`rounded-xl px-4 py-5 min-w-[80%] bg-[${props.bgColor}] my-3`}>
-            <Text className="font-bold justify-start pb-2 text-[#F1E0C5] font-montserrat">{props.noteTitle}</Text>
-            {props.noteBody.map((e, i) => {
-                return <Text className={`text-[${props.textColor}] font-montserrat`} key={i}>{e}</Text>
-            })}
+        <View className={`rounded-xl px-4 py-5 min-w-[80%] bg-[${bgColor}] my-3`}>
+            <Text className="font-bold justify-start pb-2 text-[#F1E0C5] font-montserrat">{noteTitle}</Text>
+
+            return <Text className={`text-[${textColor}] font-montserrat`}>{noteBody}</Text>
         </View>
     )
 }
