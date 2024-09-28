2325e575c018819b21a11a1079b0f62079039872
1arijz <arizukerman1@gmail.com>
2024-09-28 02:04:33 -0500

    Changed API for NoteCard

--- components/NoteCard.tsx
+++ components/NoteCard.tsx
@@ -7,12 +7,12 @@ export interface NoteCardProps {
     textColor: string;
 }
 
+// bgColor and textColor take in full tailwind string literals, eg "bg-[#71816D]". "
 function NoteCard({noteTitle, noteBody, bgColor, textColor}: NoteCardProps) {
     return (
-        <View className={`rounded-xl px-4 py-5 w-[80%] bg-[${bgColor}] my-3`}>
-            <Text className={`font-bold justify-start pb-2 text-[${textColor}] font-montserrat`}>{noteTitle}</Text>
-
-            return <Text className={`text-[${textColor}] font-montserrat`}>{noteBody}</Text>
+        <View className={`rounded-xl px-4 py-5 min-w-[80%] ${bgColor} my-3`}>
+            <Text className={`font-bold justify-start pb-2 ${textColor} font-montserrat`}>{noteTitle}</Text>
+            <Text className={`${textColor} font-montserrat`}>{noteBody}</Text>
         </View>
     )
 }
