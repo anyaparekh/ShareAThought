c510953f5540f1e06e46ddb81c06e11a14beb036
1arijz <arizukerman1@gmail.com>
2024-09-28 01:21:39 -0500

    Made title use textColor prop

--- components/NoteCard.tsx
+++ components/NoteCard.tsx
@@ -10,7 +10,7 @@ export interface NoteCardProps {
 function NoteCard({noteTitle, noteBody, bgColor, textColor}: NoteCardProps) {
     return (
         <View className={`rounded-xl px-4 py-5 w-[80%] bg-[${bgColor}] my-3`}>
-            <Text className="font-bold justify-start pb-2 text-[#F1E0C5] font-montserrat">{noteTitle}</Text>
+            <Text className={`font-bold justify-start pb-2 text-[${textColor}] font-montserrat`}>{noteTitle}</Text>
 
             return <Text className={`text-[${textColor}] font-montserrat`}>{noteBody}</Text>
         </View>
