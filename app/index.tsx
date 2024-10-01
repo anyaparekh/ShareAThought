0958901741855a4baf50c2aa05903239bf3f41dd
EvanJ <evanj3@illinois.edu>
2024-10-01 16:23:31 -0500

    removed testing code

--- app/index.tsx
+++ app/index.tsx
@@ -5,10 +5,6 @@ export default function Index() {
   return (
     <View className="bg-[#F8F0E2] w-full h-full justify-center items-center">
       <Text className="font-montserrat">Hello!</Text>
-      <TextInputField
-        title="Email Address"
-        placeholder="Enter your Email Adress"
-      />
     </View>
   );
 }
